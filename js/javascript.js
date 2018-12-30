76c20054810cc4b3b01d354b2e335b3583c016eb
Victoria Tassie <vicky_t_14@hotmail.co.uk>
2018-12-30 23:24:56 +0000

    Changed theme to incorporate bootstrap

---
+++ js/javascript.js
@@ -0,0 +1,75 @@
+<!-- Typing Text -->
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+	
+
+function slideOpen(Name, width) {
+    var x = document.getElementById(Name);
+    if (x.style.opacity === "0") {
+        //x.style.transform = "scaleY(1) scaleX(1)";
+		x.style.transition = "width 0.5s 0.4s, opacity 0.4s 0.5s";
+		x.style.width = width;
+		x.style.opacity = "1";
+        
+    } else {
+        //x.style.transform = "scaleY(0) scaleX(0)";
+		x.style.transition = "width 0.5s 0.4s, opacity 0.4s  0s";
+		x.style.width = "0";
+		x.style.opacity = "0";
+        
+    }
+}
